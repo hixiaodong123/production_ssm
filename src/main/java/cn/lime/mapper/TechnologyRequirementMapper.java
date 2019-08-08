1790950167c76fce2b90c1bd256ccaf4753ead19
hixiaodong123 <420350696@qq.com>
2019-08-08 15:31:01 +0800

    添加了一些实体类

---
+++ src/main/java/cn/lime/mapper/TechnologyRequirementMapper.java
@@ -0,0 +1,17 @@
+package cn.lime.mapper;
+
+import cn.lime.entity.TechnologyRequirement;
+
+public interface TechnologyRequirementMapper {
+    int deleteByPrimaryKey(String technologyRequirementId);
+
+    int insert(TechnologyRequirement record);
+
+    int insertSelective(TechnologyRequirement record);
+
+    TechnologyRequirement selectByPrimaryKey(String technologyRequirementId);
+
+    int updateByPrimaryKeySelective(TechnologyRequirement record);
+
+    int updateByPrimaryKey(TechnologyRequirement record);
+}
