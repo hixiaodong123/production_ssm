e168129856336c2e1531774274617136d2163ca2
hixiaodong123 <420350696@qq.com>
2019-08-08 11:47:19 +0800

    初始化框架

---
+++ src/test/java/cn/lime/test/mapper/TestCustomMapper.java
@@ -0,0 +1,28 @@
+package cn.lime.test.mapper;
+import cn.lime.entity.Custom;
+import cn.lime.mapper.CustomMapper;
+import cn.lime.test.BaseTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+/**
+ * @description: 客户测试持久层
+ * @author: Lime
+ * @create: 2019-08-08 11:22
+ **/
+
+public class TestCustomMapper extends BaseTest
+{
+
+    @Autowired
+    private CustomMapper customMapper;
+
+    @Test
+    public void testFindById()
+    {
+        Custom custom = customMapper.selectByPrimaryKey("001");
+        System.out.println(custom);
+
+    }
+}
