84addf253f543516c7313ffef2e1cd9dbb248f9b
hixiaodong123 <420350696@qq.com>
2019-08-08 15:12:31 +0800

    添加了一些实体类

---
+++ src/main/java/cn/lime/mapper/ManufactureMapper.java
@@ -0,0 +1,17 @@
+package cn.lime.mapper;
+
+import cn.lime.entity.Manufacture;
+
+public interface ManufactureMapper {
+    int deleteByPrimaryKey(String manufactureSn);
+
+    int insert(Manufacture record);
+
+    int insertSelective(Manufacture record);
+
+    Manufacture selectByPrimaryKey(String manufactureSn);
+
+    int updateByPrimaryKeySelective(Manufacture record);
+
+    int updateByPrimaryKey(Manufacture record);
+}
